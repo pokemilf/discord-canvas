e53043ff4281a679a27e2a010db8258fcf728e4d
sinkaroid <bibsindra@gmail.com>
2022-05-08 11:09:02 +0700

    feat: make canvas darker and strokeable

--- src/rank/Rank.js
+++ src/rank/Rank.js
@@ -1,7 +1,5 @@
 const Canvas = require("canvas");
-const { formatVariable, applyText } = require("../../utils/functions");
-
-const badges = [1,9]; // min, max
+const { formatVariable, applyText, randomColor } = require("../../utils/functions");
 
 module.exports = class RankCard {
   constructor() {
@@ -41,7 +39,15 @@ module.exports = class RankCard {
     this.textLevel = "lvl {level}";
     this.textNeededXp = "{current}/{needed} for next rank";
     this.textReputation = "+{reputation} rep";
-    for(let i = badges[0]; i <= badges[1]; i++) this[`badge${i}`] = null;
+    this.badge1 = null;
+    this.badge2 = null;
+    this.badge3 = null;
+    this.badge4 = null;
+    this.badge5 = null;
+    this.badge6 = null;
+    this.badge7 = null;
+    this.badge8 = null;
+    this.badge9 = null;
   }
 
   setAvatar(value) {
@@ -110,13 +116,18 @@ module.exports = class RankCard {
   }
 
   setBadge(variable, value) {
-    const number = Number(variable);
-    for(let i = badges[0]; i <= badges[1]; i++)
-      if(number === i){
-        this[`badge${number}`] = value;
-        break;
-      }
-    return this;
+    if (Number(variable) > 0 && Number(variable) < 10) {
+      if (Number(variable) === 1) this.badge1 = value;
+      if (Number(variable) === 2) this.badge2 = value;
+      if (Number(variable) === 3) this.badge3 = value;
+      if (Number(variable) === 4) this.badge4 = value;
+      if (Number(variable) === 5) this.badge5 = value;
+      if (Number(variable) === 6) this.badge6 = value;
+      if (Number(variable) === 7) this.badge7 = value;
+      if (Number(variable) === 8) this.badge8 = value;
+      if (Number(variable) === 9) this.badge9 = value;
+      return this;
+    }
   }
 
   setRadius(value) {
@@ -155,6 +166,8 @@ module.exports = class RankCard {
     ctx.fillStyle = this.colorBackground;
     ctx.fillRect(0, 0, 1080, 400);
     let background = await Canvas.loadImage(this.backgroundImage);
+
+    ctx.globalAlpha = 0.6;
     ctx.drawImage(background, 0, 0, 1080, 400);
     ctx.restore();
 
@@ -196,18 +209,31 @@ module.exports = class RankCard {
     ctx.font = applyText(canvas, this.username, 45, 460, "Bold");
     ctx.fillText(this.username, 50 + 240 + 45 + 5, 80);
 
+    ctx.lineWidth = 1
+    ctx.strokeStyle = randomColor();
+    ctx.strokeText(this.username, 50 + 240 + 45 + 5, 80);
+
     // Rank
     if (this.addonRank) {
       ctx.textAlign = "right";
       ctx.fillStyle = this.colorRank;
+
       ctx.font = applyText(canvas, "#" + this.rank, 45, 194, "Bold");
       ctx.fillText("#" + this.rank, canvas.width - 50 - 5, 80);
+
+      ctx.lineWidth = 1
+      ctx.strokeStyle = "crimson";
+      ctx.strokeText("#" + this.rank, canvas.width - 50 - 5, 80);
     }
     if (this.addonRankName) {
       ctx.textAlign = "left";
       ctx.fillStyle = this.colorRankName;
       ctx.font = applyText(canvas, this.rankName, 35, 690, "Bold");
       ctx.fillText(this.rankName, 50 + 240 + 45 + 5, 80 + 45 + 15);
+
+      ctx.lineWidth = 1
+      ctx.strokeStyle = "gold"
+      ctx.strokeText(this.rankName, 50 + 240 + 45 + 5, 80 + 45 + 15);
     }
 
     // Badges
@@ -216,26 +242,314 @@ module.exports = class RankCard {
       ctx.globalAlpha = this.opacityBadges;
       ctx.fillRect(240 + 50 + 50, 295, 700, 70);
       ctx.fillStyle = this.colorNoBadges;
-      const badgeNames = [
-        'bubble',   'speaker',  'stream',
-        'picture',  'like',     'star',
-        'boost',    'money',    'bot'
-      ];
-      for(let index = 0; index < badgeNames.length; index++){
-        let badge = `badge${index+1}`
-        if (!this[badge]){
-          ctx.globalAlpha = this.opacityNoBadges;
-          ctx.textAlign = "center";
-          ctx.font = applyText(canvas, ".", 120, 50, "Bold");
-          ctx.fillText(".", 75 * index + 390, 340);
-        }else{
-          ctx.globalAlpha = 1;
-          let badgeImg = await Canvas.loadImage(
-            ['bronze','silver','gold','diamond'].includes(this[badge].toLowerCase()) ?
-            `${__dirname}/../../assets/img/rank/${badgeNames[index]}_${this[badge].toLowerCase()}.png` :
-            this[badge]
+      if (!this.badge1) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(".", 240 + 50 + 50 + 25 + 50 / 2, 295 + 10 + 35);
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge1.toLowerCase() === "bronze" ||
+          this.badge1.toLowerCase() === "silver" ||
+          this.badge1.toLowerCase() === "gold" ||
+          this.badge1.toLowerCase() === "diamond"
+        ) {
+          let badge1 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/bubble_${this.badge1.toLowerCase()}.png`
           );
-          ctx.drawImage(badgeImg, 75 * index + 365, 305, 50, 50);
+          ctx.drawImage(badge1, 240 + 50 + 50 + 25, 295 + 10, 50, 50);
+        } else {
+          let badge1 = await Canvas.loadImage(this.badge1);
+          ctx.drawImage(badge1, 240 + 50 + 50 + 25, 295 + 10, 50, 50);
+        }
+      }
+      if (!this.badge2) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(".", 240 + 50 + 50 + 25 + 50 + 25 + 50 / 2, 295 + 10 + 35);
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge2.toLowerCase() === "bronze" ||
+          this.badge2.toLowerCase() === "silver" ||
+          this.badge2.toLowerCase() === "gold" ||
+          this.badge2.toLowerCase() === "diamond"
+        ) {
+          let badge2 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/speaker_${this.badge2.toLowerCase()}.png`
+          );
+          ctx.drawImage(badge2, 240 + 50 + 50 + 25 + 50 + 25, 295 + 10, 50, 50);
+        } else {
+          let badge2 = await Canvas.loadImage(this.badge2);
+          ctx.drawImage(badge2, 240 + 50 + 50 + 25 + 50 + 25, 295 + 10, 50, 50);
+        }
+      }
+      if (!this.badge3) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 2 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge3.toLowerCase() === "bronze" ||
+          this.badge3.toLowerCase() === "silver" ||
+          this.badge3.toLowerCase() === "gold" ||
+          this.badge3.toLowerCase() === "diamond"
+        ) {
+          let badge3 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/stream_${this.badge3.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge3,
+            240 + 50 + 50 + 25 + (50 + 25) * 2,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge3 = await Canvas.loadImage(this.badge3);
+          ctx.drawImage(
+            badge3,
+            240 + 50 + 50 + 25 + (50 + 25) * 2,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge4) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 3 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge4.toLowerCase() === "bronze" ||
+          this.badge4.toLowerCase() === "silver" ||
+          this.badge4.toLowerCase() === "gold" ||
+          this.badge4.toLowerCase() === "diamond"
+        ) {
+          let badge4 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/picture_${this.badge4.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge4,
+            240 + 50 + 50 + 25 + (50 + 25) * 3,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge4 = await Canvas.loadImage(this.badge4);
+          ctx.drawImage(
+            badge4,
+            240 + 50 + 50 + 25 + (50 + 25) * 3,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge5) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 4 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge5.toLowerCase() === "bronze" ||
+          this.badge5.toLowerCase() === "silver" ||
+          this.badge5.toLowerCase() === "gold" ||
+          this.badge5.toLowerCase() === "diamond"
+        ) {
+          let badge5 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/like_${this.badge5.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge5,
+            240 + 50 + 50 + 25 + (50 + 25) * 4,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge5 = await Canvas.loadImage(this.badge5);
+          ctx.drawImage(
+            badge5,
+            240 + 50 + 50 + 25 + (50 + 25) * 4,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge6) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 5 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge6.toLowerCase() === "bronze" ||
+          this.badge6.toLowerCase() === "silver" ||
+          this.badge6.toLowerCase() === "gold" ||
+          this.badge6.toLowerCase() === "diamond"
+        ) {
+          let badge6 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/star_${this.badge6.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge6,
+            240 + 50 + 50 + 25 + (50 + 25) * 5,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge6 = await Canvas.loadImage(this.badge6);
+          ctx.drawImage(
+            badge6,
+            240 + 50 + 50 + 25 + (50 + 25) * 5,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge7) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 6 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge7.toLowerCase() === "bronze" ||
+          this.badge7.toLowerCase() === "silver" ||
+          this.badge7.toLowerCase() === "gold" ||
+          this.badge7.toLowerCase() === "diamond"
+        ) {
+          let badge7 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/boost_${this.badge7.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge7,
+            240 + 50 + 50 + 25 + (50 + 25) * 6,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge7 = await Canvas.loadImage(this.badge7);
+          ctx.drawImage(
+            badge7,
+            240 + 50 + 50 + 25 + (50 + 25) * 6,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge8) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 7 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge8.toLowerCase() === "bronze" ||
+          this.badge8.toLowerCase() === "silver" ||
+          this.badge8.toLowerCase() === "gold" ||
+          this.badge8.toLowerCase() === "diamond"
+        ) {
+          let badge8 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/money_${this.badge8.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge8,
+            240 + 50 + 50 + 25 + (50 + 25) * 7,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge8 = await Canvas.loadImage(this.badge8);
+          ctx.drawImage(
+            badge8,
+            240 + 50 + 50 + 25 + (50 + 25) * 7,
+            295 + 10,
+            50,
+            50
+          );
+        }
+      }
+      if (!this.badge9) {
+        ctx.globalAlpha = this.opacityNoBadges;
+        ctx.textAlign = "center";
+        ctx.font = applyText(canvas, ".", 120, 50, "Bold");
+        ctx.fillText(
+          ".",
+          240 + 50 + 50 + 25 + (50 + 25) * 8 + 50 / 2,
+          295 + 10 + 35
+        );
+      } else {
+        ctx.globalAlpha = 1;
+        if (
+          this.badge9.toLowerCase() === "bronze" ||
+          this.badge9.toLowerCase() === "silver" ||
+          this.badge9.toLowerCase() === "gold" ||
+          this.badge9.toLowerCase() === "diamond"
+        ) {
+          let badge9 = await Canvas.loadImage(
+            `${__dirname}/../../assets/img/rank/bot_${this.badge9.toLowerCase()}.png`
+          );
+          ctx.drawImage(
+            badge9,
+            240 + 50 + 50 + 25 + (50 + 25) * 8,
+            295 + 10,
+            50,
+            50
+          );
+        } else {
+          let badge9 = await Canvas.loadImage(this.badge9);
+          ctx.drawImage(
+            badge9,
+            240 + 50 + 50 + 25 + (50 + 25) * 8,
+            295 + 10,
+            50,
+            50
+          );
         }
       }
     }
@@ -249,12 +563,17 @@ module.exports = class RankCard {
       .replace(/{latest}/g, latestXP);
     ctx.textAlign = "center";
     ctx.fillStyle = this.colorNeededXp;
-    ctx.font = applyText(canvas, textXPEdited, 25, 690, "Bold");
+    
+    //set canvas 50% blurry to make it look like it's being drawn
+    ctx.imageSmoothingEnabled = false;
+
+    ctx.font = applyText(canvas, textXPEdited, 28, 690, "Bold");
     ctx.fillText(
       textXPEdited,
       50 + 240 + 45 + 5 + 690 / 2,
       80 + 45 + 15 + 30 + 90
     );
+
     ctx.beginPath();
     ctx.moveTo(240 + 50 + 50 + 25, 80 + 45 + 10 + 40);
     ctx.lineTo(240 + 50 + 50 + 700 - 25, 80 + 45 + 10 + 40);
